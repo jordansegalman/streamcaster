6d7da17279f929aaf5a234c497a0b152411a12d0
Jordan Segalman <jordansegalman@gmail.com>
2018-04-13 16:32:26 -0400

    Add login API request

--- server/index.js
+++ server/index.js
@@ -87,6 +87,22 @@ app.post('/api/register', function (req, res) {
 	register(req.body.username, req.body.password, res);
 });
 
+// Caled when user attempts to log in
+app.post('/api/login', function (req, res) {
+	if (!req.body || !req.body.username || !req.body.password) {
+		return res.status(400).json({response: 'Invalid POST request'});
+	}
+	// Validate username
+	if (!validateUsername(req.body.username)) {
+		return res.status(400).json({response: 'Invalid username'});
+	}
+	// Validate password
+	if (!validatePassword(req.body.password)) {
+		return res.status(400).json({response: 'Invalid password'});
+	}
+	login(req.body.username, req.body.password, res);
+});
+
 // Validates a user ID
 function validateUid(uid) {
 	return !validator.isEmpty(uid) && validator.isHexadecimal(uid) && validator.isLength(uid, {min: 16, max: 16});
@@ -139,4 +155,27 @@ function register(username, password, res) {
 			});
 		}
 	});
+}
+
+// Logs a user in
+function login(username, password, res) {
+        // Get user ID and password hash for username
+        var sql = 'SELECT ??,?? FROM ?? WHERE ??=?';
+        var inserts = ['uid', 'password', 'accounts', 'username', username];
+        dbConnection.query(sql, inserts, function (error, results) {
+                if (error) throw error;
+                if (results.length != 1) {
+                        return res.status(400).json({response: 'Invalid username or password'});
+                } else {
+                        // Compare sent password hash to account password hash
+                        bcrypt.compare(password, result[0].password, function (err, result) {
+                                if (result === true) {
+                                        console.log("User logged in.");
+                                        return res.status(200).json({response: 'Login successful'});
+                                } else {
+                                        return res.status(400).json({response: 'Invalid username or password'});
+                                }
+                        });
+                }
+        });
 }
